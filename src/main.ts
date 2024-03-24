50de43d0d83f6bf5bfd60688bb29c8643a608b74
Auxior <skossm@outlook.com>
2024-03-24 23:44:39 +0800

    Add HttpExceptionFilter and TransformInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform/transform.interceptor';
+import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api'); // 路由全局前缀
+
+  app.useGlobalInterceptors(new TransformInterceptor()); // 全局拦截器
+
+  app.useGlobalFilters(new HttpExceptionFilter()); // 全局过滤器
+
   await app.listen(3005);
 }
 bootstrap();
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, Query, Param, Patch, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Patch,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
+  @Get('test')
+  getTest(): string {
+    throw new HttpException('获取数据失败', HttpStatus.BAD_REQUEST);
+  }
+
   @Get()
   findAll(
     @Query('keyword') keyword: string,
