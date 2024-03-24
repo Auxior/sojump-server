ed68e7ac0507759de09d85d9e86799975ab174db
Auxior <skossm@outlook.com>
2024-03-24 23:25:46 +0800

    Add QuestionDto and update QuestionController

---
+++ src/question/dto/question.dto.ts
@@ -0,0 +1,4 @@
+export class QuestionDto {
+  readonly title: string;
+  readonly desc: string;
+}
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Param, Patch, Body } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
 
 @Controller('question')
 export class QuestionController {
   @Get()
-  findAll() {
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    console.log(keyword, page, pageSize);
+
     return {
       list: ['a', 'b', 'c'],
       count: 10,
+    };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return {
+      id,
+      title: 'aaa',
+      desc: 'bbb',
+    };
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    console.log('questionDto', questionDto);
+
+    return {
+      id,
+      title: 'aaa',
+      desc: 'bbb',
     };
   }
 }
