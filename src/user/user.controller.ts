3724db9e6575ee007107fe3e91d63178d9239257
Auxior <skossm@outlook.com>
2024-03-26 00:32:07 +0800

    Add public routes for user info and login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,14 +4,18 @@ import {
   Post,
   HttpException,
   HttpStatus,
+  Get,
+  Redirect,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() userDto: CreateUserDto) {
     try {
@@ -19,5 +23,18 @@ export class UserController {
     } catch (err) {
       throw new HttpException(err, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Get('info')
+  @Redirect('/api/auth/profile', 302) // http 状态码，GET 请求 - 301 永久，302 临时
+  async info() {
+    return;
+  }
+
+  @Public()
+  @Post('login')
+  @Redirect('/api/auth/login', 307) // http 状态码，POST 请求 - 308 永久，307 临时
+  async login() {
+    return;
   }
 }
