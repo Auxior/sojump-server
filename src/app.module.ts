e5fb819e0897a1643d6484bd9a01ba9d6eeb10f7
Auxior <skossm@outlook.com>
2024-03-25 21:38:59 +0800

    Add cSpell configuration and update dependencies***
    ***Add Mongoose and ConfigModule imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+    ),
+    QuestionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
