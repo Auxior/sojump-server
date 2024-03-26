fb7229196b3bcada2adff8aebf7fde55f07febcd
Auxior <skossm@outlook.com>
2024-03-26 23:11:45 +0800

    Add getQuestionStat endpoint to StatController and implement getQuestionStatListAndCount method in StatService

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
+import { StatService } from './stat.service';
 
 @Controller('stat')
-export class StatController {}
+export class StatController {
+  constructor(private readonly statService: StatService) {}
+
+  @Get(':questionId')
+  async getQuestionStat(
+    @Param('questionId') questionId: string,
+    @Query('page') page: number = 1,
+    @Query('pageSize') pageSize: number = 10,
+  ) {
+    return await this.statService.getQuestionStatListAndCount(questionId, {
+      page,
+      pageSize,
+    });
+  }
+}
--- src/stat/stat.service.ts
+++ src/stat/stat.service.ts
@@ -1,4 +1,103 @@
 import { Injectable } from '@nestjs/common';
+import { QuestionService } from 'src/question/question.service';
+import { AnswerService } from 'src/answer/answer.service';
 
 @Injectable()
-export class StatService {}
+export class StatService {
+  constructor(
+    private readonly questionService: QuestionService,
+    private readonly answerService: AnswerService,
+  ) {}
+
+  private _getRadioOptText(value, props: any = {}) {
+    const { options = [] } = props;
+    const length = options.length;
+
+    for (let i = 0; i < length; i++) {
+      const item = options[i];
+      if (item.value === value) {
+        return item.text;
+        break;
+      }
+    }
+    return '';
+  }
+
+  private _getCheckboxOptText(value, props: any = {}) {
+    const { list = [] } = props;
+    const length = list.length;
+
+    for (let i = 0; i < length; i++) {
+      const item = list[i];
+      if (item.value === value) {
+        return item.text;
+        break;
+      }
+    }
+    return '';
+  }
+
+  /**
+   * 生成答案信息，格式如 { componentFeId1: value1, componentFeId2: value2 }
+   * @param {Object} question question info
+   * @param {Array} answerList answer list
+   */
+  private _genAnswersInfo(question, answerList = []) {
+    const res = {};
+
+    const { componentList = [] } = question;
+
+    answerList.forEach((a) => {
+      const { componentFeId, value = [] } = a;
+
+      // 获取组件信息
+      const comp = componentList.filter((c) => c.fe_id === componentFeId)[0];
+      const { type, props = {} } = comp;
+      if (type === 'questionRadio') {
+        // 单选
+        res[componentFeId] = value
+          .map((v) => this._getRadioOptText(v, props))
+          .toString();
+      } else if (type === 'questionCheckbox') {
+        // 多选
+        res[componentFeId] = value
+          .map((v) => this._getCheckboxOptText(v, props))
+          .toString();
+      } else {
+        // 其他
+        res[componentFeId] = value.toString();
+      }
+    });
+
+    return res;
+  }
+
+  // 获取单个问卷的答卷列表 (分页) 和数量
+  async getQuestionStatListAndCount(
+    questionId: string,
+    opt: { page: number; pageSize: number },
+  ) {
+    const noData = { list: [], count: 0 };
+    if (!questionId) return noData;
+
+    const q = await this.questionService.findOne(questionId);
+    if (q == null) return noData;
+
+    const total = await this.answerService.count(questionId);
+    if (total === 0) return noData;
+
+    const answers = await this.answerService.findAll(questionId, opt);
+
+    const list = answers.map((a) => {
+      return {
+        _id: a._id,
+        ...this._genAnswersInfo(q, a.answerList),
+      };
+    });
+
+    return {
+      list,
+      total,
+    };
+  }
+}
